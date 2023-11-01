8da09a1a49f2d3e6abe8c4e7a66ef83e40ea9938
Mahesh <mahesh@Maheshs-MacBook-Pro.local>
2023-11-01 20:55:11 +0530

    404.js

--- src/pages/noPage/NoPage.jsx
+++ src/pages/noPage/NoPage.jsx
@@ -1,11 +1,93 @@
-import React from 'react'
+import { useContext, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import myContext from '../../context/data/myContext'
+import Loader from '../../components/loader/Loader';
+import { auth ,fireDB} from '../../firebase/FireBaseConfig';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { toast } from 'react-toastify';
 
-function NoPage() {
-  return (
-    <div>
-      No Page
-    </div>
-  )
+
+function Login() {
+
+    const context = useContext(myContext);
+    const {loading,setLoading} = context;
+    const navigate = useNavigate();
+
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+
+    const login=async()=>{
+        setLoading(true)
+        try {
+            const result  = await signInWithEmailAndPassword(auth,email,password);
+            localStorage.setItem('user',JSON.stringify(result))
+            toast.success('login successfully!',{
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: true,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "colored",
+              })
+            setLoading(false);
+            navigate('/')
+
+        } catch (error) {
+            setLoading(false);
+            toast.error(error,{
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: true,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "colored",
+              })
+            console.log(error)
+        }
+    }
+   
+    return (
+        <div className=' flex justify-center items-center h-screen'>
+            {loading && <Loader/>}
+            <div className=' bg-gray-800 px-10 py-10 rounded-xl '>
+                <div className="">
+                    <h1 className='text-center text-white text-xl mb-4 font-bold'>Login</h1>
+                </div>
+                <div>
+                    <input type="email"
+                    value={email}
+                    onChange={(e)=>setEmail(e.target.value)}
+                        name='email'
+                        className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
+                        placeholder='Email'
+                    />
+                </div>
+                <div>
+                    <input
+                     value={password}
+                     onChange={(e)=>setPassword(e.target.value)}
+                        type="password"
+                        className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
+                        placeholder='Password'
+                    />
+                </div>
+                <div className=' flex justify-center mb-3'>
+                    <button
+                    onClick={login}
+                        className=' bg-yellow-500 w-full text-black font-bold  px-2 py-2 rounded-lg'>
+                        Login
+                    </button>
+                </div>
+                <div>
+                    <h2 className='text-white'>Don't have an account <Link className=' text-yellow-500 font-bold' to={'/signup'}>Signup</Link></h2>
+                </div>
+            </div>
+        </div>
+    )
 }
 
-export default NoPage
+export default Login
